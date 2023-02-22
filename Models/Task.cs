35605421c5a1fa880d27b4f0618b20952ba3e562
Jackson Huish <jacksonhuish88@gmail.com>
2023-02-22 10:27:31 -0700

    seeded data, migrations and database

---
+++ Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_0208.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+
+        [Required]
+        public string TaskName { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+
+        // FK Relationship
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public bool Completed { get; set; }
+    }
+}
